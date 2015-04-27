7ebaf55267476f570fb7dbee13ba94d7a999ad4a
doronrk <doronrk@gmail.com>
2015-04-26 23:56:52 -0400

    initial commit

--- about_types.go
+++ about_types.go
@@ -1,13 +1,22 @@
 package go_koans
 
+
 type coolNumber int
+type coolString string
 
 func (cn coolNumber) multiplyByTwo() int {
-	return int(cn) * 2
+	return int(cn * 2)
 }
+
+func (cs coolString) double() string {
+    return string(cs + cs)
+}
+
 
 func aboutTypes() {
 	i := coolNumber(4)
-	assert(i == coolNumber(__int__))     // values can be converted between compatible types
-	assert(i.multiplyByTwo() == __int__) // you can add methods on any type you define
+	assert(i == coolNumber(4))     // values can be converted between compatible types
+	assert(i.multiplyByTwo() == 8) // you can add methods on any type you define
+    s := coolString("hello")
+    assert(s.double() == "hellohello")
 }
